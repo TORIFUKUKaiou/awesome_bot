495b87369964b4a9b1965764ec7b5889a2568104
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2020-10-11 09:14:14 +0900

    add config

--- config/config.exs
+++ config/config.exs
@@ -5,7 +5,20 @@
 # is restricted to this project.
 import Config
 
-config :awesome_bot, target: Mix.target()
+config :awesome_bot,
+  target: Mix.target(),
+  twitter_query: System.get_env("AWESOME_BOT_TWITTER_QUERY"),
+  twitter_search_interval:
+    System.get_env("AWESOME_BOT_TWITTER_SEARCH_INTERVAL") |> String.to_integer(),
+  slack_incoming_webhook_url: System.get_env("AWESOME_BOT_SLACK_INCOMING_WEBHOOK_URL"),
+  slack_channel: System.get_env("AWESOME_BOT_SLACK_CHANNEL"),
+  docomo_text_to_speech_api_key: System.get_env("AWESOME_BOT_DOCOMO_TEXT_TO_SPEECH_API_KEY")
+
+config :extwitter, :oauth,
+  consumer_key: System.get_env("AWESOME_BOT_TWITTER_CONSUMER_KEY"),
+  consumer_secret: System.get_env("AWESOME_BOT_TWITTER_CONSUMER_SECRET"),
+  access_token: System.get_env("AWESOME_BOT_TWITTER_ACCESS_TOKEN"),
+  access_token_secret: System.get_env("AWESOME_BOT_TWITTER_ACCESS_TOKEN_SECRET")
 
 # Customize non-Elixir parts of the firmware. See
 # https://hexdocs.pm/nerves/advanced-configuration.html for details.
