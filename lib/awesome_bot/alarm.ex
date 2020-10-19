2b3802fba01d68ec13e79fdc5e148be1dea44215
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2020-10-19 23:24:55 +0900

    play cmd

--- lib/awesome_bot/alarm.ex
+++ lib/awesome_bot/alarm.ex
@@ -6,7 +6,11 @@ defmodule AwesomeBot.Alarm do
                else: "/tmp/alarm.wav"
   @play_cmd if(AwesomeBot.Application.target() == :host,
               # host
-              do: "afplay #{@file_path}",
+              do:
+                if(:os.type() == {:unix, :darwin},
+                  do: "afplay #{@file_path}",
+                  else: "aplay -q #{@file_path}"
+                ),
               # target
               else: "aplay -q #{@file_path}"
             )
